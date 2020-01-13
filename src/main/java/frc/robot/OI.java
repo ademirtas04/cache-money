2580f97655f4c931c880ff6dd70e1f2d0a61bcb5
ademirtas04 <59454552+ademirtas04@users.noreply.github.com>
2020-01-13 16:58:05 -0600

    BIG DAWGS

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -1,11 +1,21 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import frc.robot.commands.MoveSequence;
+import frc.robot.commands.NewMove;
 
 
 public class OI {
-
     private final Joystick driveController = new Joystick(RobotMap.DRIVER_CONTROLLER);
+    Button xButton = new JoystickButton(driveController, RobotMap.BUTTON_X);
+
+
+    public OI() {
+        xButton.whenPressed(new MoveSequence());
+    }
+
 
     public double getDriverRawAxis(int axis) {
         return driveController.getRawAxis(axis);
