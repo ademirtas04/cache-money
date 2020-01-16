ba2bfdbee4cc49adbe45965f47003e520b7f09dc
ademirtas04 <59454552+ademirtas04@users.noreply.github.com>
2020-01-16 16:47:50 -0600

    Autonomous Straight Line

---
+++ src/main/java/frc/robot/commands/Autonomous.java
@@ -0,0 +1,56 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Robot;
+
+public class Autonomous extends Command {
+  private double startTime;
+  public Autonomous() {
+    // Use requires() here to declare subsystem dependencies
+    // eg. requires(chassis);
+  }
+
+  // Called just before this Command runs the first time
+  @Override
+  protected void initialize() {
+    startTime = Timer.getFPGATimestamp();
+  }
+
+  // Called repeatedly when this Command is scheduled to run
+  @Override
+public void execute() {
+    double time = Timer.getFPGATimestamp();
+    if (time - startTime < 3){
+      Robot.driveTrain.setLeftMotors(0.6);
+      Robot.driveTrain.setRightMotors(0.6);
+    } else {
+      Robot.driveTrain.setLeftMotors(0);
+      Robot.driveTrain.setRightMotors(0);
+    }
+  }
+
+  // Make this return true when this Command no longer needs to run execute()
+  @Override
+  protected boolean isFinished() {
+    return false;
+  }
+
+  // Called once after isFinished returns true
+  @Override
+  protected void end() {
+  }
+
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+  @Override
+  protected void interrupted() {
+  }
+}
