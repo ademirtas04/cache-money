1ba81850dc1015ee7d391f11c164bd096f81a0f0
DanielOC16 <doconnor@ucls.uchicago.edu>
2020-01-18 14:55:34 -0600

    Revert "Autonomous Straight Line"
    This reverts commit ba2bfdbee4cc49adbe45965f47003e520b7f09dc.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -11,8 +11,6 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.commands.Autonomous;
-import frc.robot.commands.TankDrive;
 import frc.robot.misc.ControlChooser;
 import frc.robot.misc.SensorReset;
 import frc.robot.misc.SmartDashboardInterface;
@@ -29,8 +27,6 @@ import frc.robot.subsystems.DriveTrain;
  */
 public class Robot extends TimedRobot {
   public static DriveTrain driveTrain = new DriveTrain();
-  public static TankDrive tankDrive = new TankDrive();
-  public static Autonomous autonomous = new Autonomous();
   public static OI m_oi;
   private Command m_autonomousCommand;
 
@@ -104,7 +100,7 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void autonomousPeriodic() {
-    autonomous.execute();
+    CommandScheduler.getInstance().run();
   }
 
   @Override
@@ -124,7 +120,7 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void teleopPeriodic() {
-    tankDrive.execute();
+    m_controlChooser.ControlSafety(SmartDashboardInterface.controlType.getSelected());
   }
 
   @Override
