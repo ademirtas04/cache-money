89b2c75e34a739d46490c2a0b3209e9034fe8680
ademirtas04 <59454552+ademirtas04@users.noreply.github.com>
2020-01-25 11:03:16 -0600

    Pixy works lol

--- src/main/java/frc/robot/commands/WheelColorIdentification.java
+++ src/main/java/frc/robot/commands/WheelColorIdentification.java
@@ -9,10 +9,14 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.pixy.Pixy2;
+import frc.robot.pixy.links.I2CLink;
+import frc.robot.pixy.links.Link;
 
 public class WheelColorIdentification extends Command {
+  Link i2c = new I2CLink();
+  Pixy2 cam = new Pixy2(i2c);
   public WheelColorIdentification() {
-    //Pixy2 cam = new Pixy2();
+    
 
     // Use requires() here to declare subsystem dependencies
     // eg. requires(chassis);
@@ -21,12 +25,13 @@ public class WheelColorIdentification extends Command {
   // Called just before this Command runs the first time
   @Override
   protected void initialize() {
-    //cam.getCCC;
+    cam.getCCC();
   }
 
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
+    cam.getCCC();
   }
 
   // Make this return true when this Command no longer needs to run execute()
@@ -34,10 +39,10 @@ public class WheelColorIdentification extends Command {
   protected boolean isFinished() {
     return false;
   }
-
   // Called once after isFinished returns true
   @Override
   protected void end() {
+    cam.close();
   }
 
   // Called when another command which requires one or more of the same
