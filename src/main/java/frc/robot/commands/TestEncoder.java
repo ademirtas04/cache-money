e1571cb68a1cede76b7ecd32c8abd4ba1c1ec21c
ademirtas04 <59454552+ademirtas04@users.noreply.github.com>
2021-02-19 17:34:59 -0600

    Update TestEncoder.java

--- src/main/java/frc/robot/commands/TestEncoder.java
+++ src/main/java/frc/robot/commands/TestEncoder.java
@@ -34,6 +34,12 @@ public class TestEncoder extends Command {
   public int direction=0;
   private double minsetpoint;
   private double maxsetpoint;
+  private int iterations = 0;
+  private double setpoint = 2;
+  private double previousDistance = 0;
+  private double distance = 0;
+  private double rate = 0;
+  private double lastTimestamp = 0;
   public TestEncoder(/*Encoder encoder1, Encoder encoder2, VictorSPX lift, VictorSPX winch, int d, double minsetpoint, double maxsetpoint*/){
     System.out.println("Constructing");
     /*this.liftEncoder = encoder1;
@@ -58,9 +64,35 @@ public class TestEncoder extends Command {
     //System.out.println(Timer.getFPGATimestamp());
     if(!initialized){
       liftEncoder = Climb.getLiftEncoder();
+      // Encoder now counts the number of revolutions
+      liftEncoder.setDistancePerPulse(1/2048);
     }
+    //Test Two: Testing if encoder counts the absolute value of the distance traveled or is direction-based
+    /*
+    if(iterations < 10) {
+      Climb.getLiftMotor().set(ControlMode.PercentOutput, 0.75); // remember to comment out the other set
+    } else if (iterations < 20 && iterations >= 10) {
+      Climb.getLiftMotor().set(ControlMode.PercentOutput, -0.75);
+    }
+    */
     Climb.getLiftMotor().set(ControlMode.PercentOutput, 0.75);
-    System.out.println(liftEncoder.get());
+    
+    //Test One: Testing if encoder counts revolutions, rate, and sees what the motor's -1.0 to 1.0 sets the velocity to
+    System.out.println("Distance: " + liftEncoder.get());
+    System.out.println("Rate: " + liftEncoder.getRate());
+
+    //Test Three: Begin to use PID to simulate motion + movement - assuming it is not absolute value
+    /*distance = liftEncoder.getDistance() * RobotMap.kDriveTick2Feet;
+
+    rate = (distance - previousDistance) / (Timer.getFPGATimestamp() - lastTimestamp);
+    lastTimestamp = Timer.getFPGATimestamp();
+
+    //We can add the Integral term later - that one will need more knowledge of what we're doing and how encoders work
+    double outputSpeed = RobotMap.kP * distance + RobotMap.kD * rate;
+    Climb.getLiftMotor().set(ControlMode.PercentOutput, outputSpeed)
+    distance = previousDistance;
+    */
+    iterations++;
   }
 
   @Override
@@ -68,6 +100,7 @@ public class TestEncoder extends Command {
     Climb.getLiftMotor().set(ControlMode.PercentOutput, 0);
     liftEncoder.reset();
     super.cancel();
+    //once we get to testing PID, set all the values here to 0
   }
 
   @Override
