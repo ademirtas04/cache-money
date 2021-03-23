2a46df06d441ce6158904fc014ed21a18d45376e
efuller83 <59541622+efuller83@users.noreply.github.com>
2021-03-23 14:31:39 -0500

    Update TankDrive.java

--- src/main/java/frc/robot/commands/TankDrive.java
+++ src/main/java/frc/robot/commands/TankDrive.java
@@ -50,12 +50,12 @@ public class TankDrive extends Command {
   
   public static void move() {
     
-    double speed = -1*Robot.m_oi.getDriverRawAxis(RobotMap.LEFT_STICK_Y);
+    double speed = 1*Robot.m_oi.getDriverRawAxis(RobotMap.LEFT_STICK_Y);
     double turn  = -0.6*Robot.m_oi.getDriverRawAxis(RobotMap.RIGHT_STICK_X);
     //The Left is pos the right is neg
     double right = speed + turn;
     double left = speed - turn;
-    DriveTrain.move(left,right);
+    DriveTrain.move(-left,-right);
     
   }
 
